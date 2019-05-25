aae4852ab40f4672a467ea7f3ff8e30117111cd6
Warren Yao <warrenyca905@gmail.com>
2019-05-25 19:15:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 class Dog
   def name=(dog_name)
-    new_name = dog_name
+    @new_name = dog_name
   end
   
   def name(dog_name)
-    new_name
+    @new_name
   end
   
   def bark
